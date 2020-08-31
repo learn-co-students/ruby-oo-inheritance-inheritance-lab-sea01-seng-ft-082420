fe05c768dcb1275a11b96856cccbe858e4201d7e
MullaN <admullan@gmail.com>
2020-08-31 16:29:02 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative './user.rb'
 
+class Student < User
+    attr_reader :knowledge
+    def initialize
+        super
+        @knowledge = []
+    end
+
+    def learn(topic)
+        @knowledge << topic
+    end
 end
