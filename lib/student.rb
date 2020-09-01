565a6c8521de50445418a36a520306f752d3a762
Arpita-22 <duttaarpita22@gmail.com>
2020-09-01 14:06:13 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
 
+    def learn(name)
+        @knowledge << name
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
