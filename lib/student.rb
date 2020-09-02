785351c2978a9890c1dbe606e4dfd88f8180ab1c
jonelle-noelani <jonelle.noelani@gmail.com>
2020-09-01 22:29:53 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
 
+    def initialize
+        @knowledge = [ ]
+    end
+  
+    def learn(piece)
+        @knowledge << piece
+    end
+
+    def knowledge
+        @knowledge
+    end
+        
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require_relative "../lib/user.rb"
 require_relative "../lib/teacher.rb"
 require_relative "../lib/student.rb"
+require 'pry'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
