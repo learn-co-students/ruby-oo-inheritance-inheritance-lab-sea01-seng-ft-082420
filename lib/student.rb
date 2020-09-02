7c1dc20c584532ba0f19ad10c7a1b7bf2a31de80
jonelle-noelani <jonelle.noelani@gmail.com>
2020-09-01 22:40:30 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,15 +1,13 @@
 class Student < User
 
+    attr_reader  :knowledge
+
     def initialize
         @knowledge = [ ]
     end
   
     def learn(piece)
         @knowledge << piece
-    end
-
-    def knowledge
-        @knowledge
     end
         
 end
